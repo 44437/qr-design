86a9230bf5b6df7662484c5664273b13955875cf
44437 <ercantopuz33@gmail.com>
2024-09-08 15:10:37 +0300

    Implement the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-
-const App = () =>
+const App = () => (
   <>
-    <p className="text-3xl">hi</p>
+    <div className="flex bg-c_blue h-full min-h-screen items-center justify-center">
+      <div className="bg-white flex flex-col w-[320px] p-4 rounded-[20px] shadow-[0px_25px_25px_rgba(0,0,0,0.048)]">
+        <img
+          className="rounded-[10px]"
+          src="./images/image-qr-code.png"
+          alt="image-qr-code.png"
+        />
+        <div className="h-[24px]"></div>
+        <div className="mx-4">
+          <p className="text-[#1F314F] font-bold text-center	text-[22px] leading-120 font-outfit">
+            Improve your front-end skills by building projects
+          </p>
+          <div className="h-[16px]"></div>
+          <p className="text-[#68778D] tracking-0.2 font-normal text-center	text-[15px] leading-140 font-outfit">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+        <div className="h-[24px]"></div>
+      </div>
+    </div>
   </>
+);
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        c_blue: "#D5E1EF",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      lineHeight: {
+        120: "120%",
+        140: "140%",
+      },
+      letterSpacing: {
+        0.2: "0.2px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
